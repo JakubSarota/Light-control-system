3926abe81227d9e2d0483124c331d69157492c49
Kubcorz <Lapek75@gmail.com>
2021-04-17 16:30:50 +0200

    #task 1
    +add logout panel
    +better login panel
    +removes "Main page" and "About us"

--- stronaGlowna.php
+++ stronaGlowna.php
@@ -13,25 +13,22 @@
     <div class="baner">
         <div class="navbar">
             <img src="./img/logo.png" class="logo" />
-            <ul>
-                <li><a href="#">Strona Główna</a></li>
-                <li><a href="#">Sterowanie</a></li>
-                <li><a href="#">Statystyki</a></li>
-                <li><a href="#">O Nas</a></li>
-
+            <ul>           
+                <li><a>
+                        <?php
+                        echo"<p>Witaj ".$_SESSION['username']."!";
+                        ?>
+                
+                </a></li>
+                <li><a href="wyloguj.php">Wyloguj</a></li>
             </ul>
         </div>
         <div class="content">
-            <h1>Oświetl swój dom</h1>
-            <p>Wyobraź sobie, że jednym dotknięciem wyłączasz wszystkie światła. Poczuj magię kina we własnym salonie <br />dzięki potędze kolorów. Albo ustaw optymalne światło, które pomoże Ci się skoncentrować lub odpocząć po długim dniu… <br />Nawet kiedy jesteś poza domem, możesz zaprogramować oświetlenie tak, by symulować obecność.</p>
             <div>
-                <button type="button"><span></span>Rozpocznij już teraz</button>
-                <button type="button"><span></span>O nas</button>
+                <button type="button"><span></span>Sterowanie oświetleniem</button>
+                <button type="button"><span></span>Statystyki</button>
             </div>
         </div>
-      
-
     </div>
-    
 </body>
 </html>
